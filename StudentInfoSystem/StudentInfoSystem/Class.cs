eeffcf8a5d6c3aae832599651159e17b90bbcadb
sevgiaykir <48965675+sevgiaykir@users.noreply.github.com>
2021-01-30 00:14:32 +0300

    Add files via upload
    Student Information System with Anonymous Types

---
+++ StudentInfoSystem/StudentInfoSystem/Class.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    public class Class
+    {
+        private List<Student> students = new List<Student>();
+        public void StudentAdd(Student student)
+        {
+            //Control student registration
+            bool isStudentExist = false;
+            foreach (var std in students)
+            {
+                if (std.Name == student.Name  && std.Surname == student.Surname)
+                {
+                    isStudentExist = true;
+                    Console.WriteLine("This student is already enrolled!");
+                }
+            }  
+            if (!isStudentExist)
+            { 
+                //Finding max student id number and assign +1 of it to new student
+                int maxId=100;
+                foreach (var std in students)
+                {
+                    if (std.Id > maxId)
+                    {
+                        maxId = std.Id;
+                        
+                    }
+                    student.Id = maxId + 1;
+                }              
+                students.Add(student);
+                Console.WriteLine("Student registration is successful!");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void RemoveStudentById(int id)
+        {
+            //Control student registration
+            var stdToRemove = students.SingleOrDefault(s => s.Id == id);
+            if (stdToRemove != null)
+            {
+                students.Remove(stdToRemove);
+                Console.WriteLine("Student Registration is removed!");
+            }               
+            else
+            {
+                Console.WriteLine("There is no student with this id!");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void ShowStudents()
+        {
+            Console.WriteLine("Id - Name - Surname");
+            foreach (var i in students)
+            {
+                Console.WriteLine($"{i.Id}- {i.Name} {i.Surname}");
+            }
+            Console.WriteLine("\n");
+        }
+        public void GetStudentsByName(string name)
+        {
+            //Control student registration
+            bool isStudentExist = false;
+            foreach (var std in students)
+            {
+                if (std.Name == name)
+                {
+                    isStudentExist = true;
+                    var findWithName = students.Where(p => p.Name.Contains(name)).ToList();
+                    Console.WriteLine("Registration is found..");
+                    findWithName.ForEach(stud => Console.WriteLine($"{stud.Id}- {stud.Name} {stud.Surname}"));
+                }
+            }
+            if(!isStudentExist)
+            {
+                Console.WriteLine("There is no student with this name!");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void GetStudentById(int id)
+        {
+            //Control student registration
+            var findWithId = students.FirstOrDefault(s => s.Id == id);
+            if (findWithId != null)
+            {
+                Console.WriteLine("Registration is found..");
+                Console.WriteLine($"{findWithId.Id}- {findWithId.Name} {findWithId.Surname}");
+            }
+            else
+            {
+                Console.WriteLine("There is no student with this id!");
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
---
+++ StudentInfoSystem/StudentInfoSystem/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentInfo = new Class();
+            studentInfo.StudentAdd(new Student { Id = 101, Name = "Sevgi", Surname = "Aykır" });
+            studentInfo.StudentAdd(new Student { Id = 102, Name = "Emir Tuna", Surname = "Boylu" });
+            studentInfo.StudentAdd(new Student { Id = 103, Name = "Nazlıcan", Surname = "Toygar" });
+            studentInfo.StudentAdd(new Student { Id = 104, Name = "Ali", Surname = "Bedez" });
+            studentInfo.StudentAdd(new Student { Id = 105, Name = "Müge", Surname = "Karabulut" });
+            studentInfo.StudentAdd(new Student { Id = 106, Name = "Kemal", Surname = "Aykır" });
+
+            Console.WriteLine("***Welcome to Student Information System!***");
+            try
+            {
+                while(true)
+                {
+                    Console.WriteLine("Please choose a operation: \n 1:Showing student list\n 2:Adding new student\n 3:Removing an existing student\n " +
+                                    "4:Getting student by name\n 5:Getting student by Id");
+                    int response = Convert.ToInt32(Console.ReadLine());
+                    switch (response)
+                    {
+                        case 1:
+                            studentInfo.ShowStudents();
+                            break;
+
+                        case 2: 
+                            Console.WriteLine("Enter the name of the student you want to add: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Enter the surname of the student you want to add: ");
+                            string surname = Console.ReadLine();
+                            studentInfo.StudentAdd(new Student { Name = name, Surname = surname });
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Enter the id of the student you want to remove:");
+                            int id = Convert.ToInt32(Console.ReadLine());
+                            studentInfo.RemoveStudentById(id);
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Enter the name of the student you want to search: ");
+                            string searchName = Console.ReadLine();
+                            studentInfo.GetStudentsByName(searchName);
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Enter the id of the student you want to search: ");
+                            int searchId = Convert.ToInt32(Console.ReadLine());
+                            studentInfo.GetStudentById(searchId);
+                            break;
+
+                        default:
+                            Console.WriteLine("Please enter a valid value!\n");
+                            break;
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Something went wrong!\n" + error.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
